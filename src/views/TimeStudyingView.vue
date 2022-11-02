5ecee966085045e51ce8cd90eb912af18b6cd963
mateoepica <mateo@epica.ai>
2022-11-01 20:29:42 -0500

    fix to add time studied

--- src/views/TimeStudyingView.vue
+++ src/views/TimeStudyingView.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1 >{{ selection }}</h1>
+        <h1 >{{ selection[0] }}</h1>
         <h1 v-if="time<60">
             {{ time }} segundos
         </h1>
@@ -9,6 +9,10 @@
         </h1>
         <el-button @click="startMeasuring">Empezar a medir</el-button>
         <select-topic @listenTopic="getTopic($event)"></select-topic>
+        <el-input type="textarea" placeholder="Añade una descripción" v-model="description"></el-input>
+        <el-tooltip content="Enviar tiempo estudiado">
+            <el-button @click="sendTimeStudied">Enviar</el-button>
+        </el-tooltip>
     </div>
 </template>
 
@@ -16,7 +20,8 @@
 import SelectTopic from '@/components/TopicComponent.vue'
 import {mapState} from 'vuex'
 import moment from 'moment'
-//import axios from 'axios'
+import axios from 'axios'
+
 export default {
     name:"TimeStudyingView",
     components:{
@@ -26,7 +31,12 @@ export default {
         return {
             
             
-            selection:'',
+            selection:[],
+            startTime:null,
+            total_time_studying_topic:0,
+            topicId:null,
+            description:'',
+            
         }
     },
     computed:{
@@ -66,9 +76,9 @@ export default {
             this.$store.commit('updateMeasure')
             console.log(this.$store.state.measure)
             if (this.$store.state.measure){
-                const startTime = moment()//.minute()
+                this.startTime = moment()//.minute()
                 console.log('startTime')
-                console.log(startTime)
+                console.log(this.startTime)
                 //console.log(typeof startTime)
             }else{
                 console.log('moment() - startTime')
@@ -76,7 +86,11 @@ export default {
                 var now = moment()
                 console.log('now')
                 console.log(now)
-                console.log(now.diff(this.startTime, 'minutes'))//.minute())
+                console.log('this.startTime')
+                console.log(this.startTime)
+                this.total_time_studying_topic += now.diff(this.startTime, 'seconds')
+                console.log(this.total_time_studying_topic)//.minute())
+                this.startTime = now 
             }
 
             this.runTime()
@@ -89,9 +103,24 @@ export default {
                 (item)=>{
                     console.log(item)
                     if(data.id==item.id)
-                    this.selection = item.topic
+                    this.selection.push(item.id)//topic
+                    //this.topicId = item.id
                 }
             )
+        },
+        sendTimeStudied(){
+            const minutes_studied = this.total_time_studying_topic/60
+            console.log(`Dummy: Abi Studied: ${this.selection} \n Time Studied: ${minutes_studied}`)
+            axios.post('http://127.0.0.1:8000/add-time-studied-topic/',{
+                
+                    topics:this.selection,
+                    time_in_minutes:minutes_studied,
+                    description:this.description
+                
+            })
+            .then(response=>{
+                console.log(response)
+            })
         }
     },
 
